debf5cac144e065331d3760eb30f2c118a5b9a23
Oakhart <voiseless@gmail.com>
2016-07-26 18:34:47 +0300

    Comments added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,41 +4,33 @@
 #include <QMessageBox>
 #include <QRadioButton>
 
-static QString radioButtonLabels[] = {
-
-    "MD4",
-    "MD5",
-    "SHA-1"
-    "SHA-224 (SHA-2)",
-    "SHA-256 (SHA-2)",
-    "SHA-384 (SHA-2)",
-    "SHA-512 (SHA-2)",
-    "SHA3-224",
-    "SHA3-256",
-    "SHA3-384",
-    "SHA3-512",
-    "MD5 and SHA-1"
-};
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    // sets up the Main Window appearance (only the default widgets, and widgets added via graphical editor)
     ui->setupUi(this);
+
+    // creates the "Please Wait" and "Result" dialogs
     pleaseWaitDialog = new PleaseWaitDialog(this);
     fileDialog = new QFileDialog(this);
+
+    // creates the timer, checking for running threads
     threadTimer = new QTimer(this);
 
-
+    // sets the timer in "pulse" mode
     threadTimer->setSingleShot(false);
 
+    // connects the "Main Window" slots (those which are not connected via the graphical editor) to the corresponding signals
     connect( pleaseWaitDialog, SIGNAL(closePleasWaitDialog()), this, SLOT(onPleaseWaitDialogClose()) );
     connect( fileDialog, SIGNAL(currentChanged(QString)), this, SLOT(onFileSelected(QString)) );
     connect( threadTimer, SIGNAL(timeout()), this, SLOT(onTick()));
 
-    ui->radioButton_SHA_1->setChecked(true);
+    // the calculating flag is set down, when ther Main Window is created
     calculating = false;
 
+    // saves pointers to ui radio buttons in the vector
     radioButtons = {
       ui->radioButton_MD4,
       ui->radioButton_MD5,
@@ -63,88 +55,138 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_toolButton_clicked()
 {
+    // displays the file dialog
     fileDialog->show();
 }
 
 void MainWindow::onFileSelected(const QString & path)
 {
+    // when a file is selected in the file dialog, the path to file is loaded into the line edit
+    // the path is not saved, because the user might want to edit it
     ui->lineEdit->setText(path);
 }
 
 void MainWindow::calculate_MD5_and_SHA1()
 {
+    //the following code runs once for MD5 and once for Sha1
     for(int i = 0; i < 2; ++i){
 
+        // creates a new object, for caclculating a checksum
         hashCalculatorVector.push_back( new HashCalculator() );
+
+        // creates a new thread, in which the object will do its calculations
         threadVector.push_back( new QThread(this) );
 
+        // gives the calculating object the name of the file, which checksum will be calculated
         hashCalculatorVector.back()->setFileName(fileName);
 
+        // sets MD5 or Sha1 hash algorithm for the calculation
         if(i == 0){
             hashCalculatorVector.back()->setHashAlgorithm(QCryptographicHash::Md5);
         } else {
             hashCalculatorVector.back()->setHashAlgorithm(QCryptographicHash::Sha1);
         }
 
+        // connects the calculating object to the corresponding thread
         hashCalculatorVector.back()->init(*threadVector.back());
+
+        // moves the calculation in the thread. Object (which inherits QObject) cannot be moved to a thread if its parent is pointed explicitly
         hashCalculatorVector.back()->moveToThread(threadVector.back());
+
+        // starts the thread
         threadVector.back()->start();
 
     }
+
+    // starts the timer, responsible for checking whether there are running threads
     threadTimer->start(500);
-        resultDialog = new ResultDialog();
+
+    // creates new Result dialog
+    resultDialog = new ResultDialog();
+
+    // sets the result labels according to the calculating algorithms
     resultDialog->setAlgorithms(QCryptographicHash::Md5, QCryptographicHash::Sha1);
 }
 
 void MainWindow::calculateSingleChecksum(QCryptographicHash::Algorithm algoritm)
 {
+
+    // creates a new object, for caclculating a checksum
     hashCalculatorVector.push_back( new HashCalculator() );
+
+    // creates a new thread, in which the object will do its calculations
     threadVector.push_back( new QThread(this) );
 
+    // gives the calculating object the name of the file, which checksum will be calculated
     hashCalculatorVector.back()->setFileName(fileName);
+
+    // sets the algorithm for the calculation
     hashCalculatorVector.back()->setHashAlgorithm(algoritm);
+
+    // connects the calculating object to the corresponding thread
     hashCalculatorVector.back()->init(*threadVector.back());
+
+    // moves the calculation in the thread. Object (which inherits QObject) cannot be moved to a thread if its parent is pointed explicitly
     hashCalculatorVector.back()->moveToThread(threadVector.back());
+
+    // starts the thread
     threadVector.back()->start();
 
+    // starts the timer, responsible for checking whether there are running threads
     threadTimer->start(500);
+
+    // creates new Result dialog
     resultDialog = new ResultDialog();
+
+    // sets the result label according to the calculating algorithms
     resultDialog->setAlgorithms(algoritm);
 }
 
 
 void MainWindow::on_pushButton_clicked()
 {
-    if(!calculating){
-
-        hashCalculatorVector.clear();
-        threadVector.clear();
-
-        fileName = ui->lineEdit->text();
-        if(fileName.size() == 0){
-            QMessageBox::warning(this, "File Not Selected", "There is no file selected");
-            return;
-        }
-        QFileInfo check(fileName);
-        if(!check.exists() || !check.isFile()){
-            QMessageBox::warning(this, "File does not exist!", fileName + " does not exist");
-            return;
-        }
-
-        for(int i = 0; i < SHA1_AND_MD5; ++i){
-            if( radioButtons[i]->isChecked() ){
-                calculateSingleChecksum( static_cast<QCryptographicHash::Algorithm>(i) );
-            }
-        }
-
-        if(radioButtons[SHA1_AND_MD5]->isChecked()){
-            calculate_MD5_and_SHA1();
-        }
-
-        pleaseWaitDialog->reinit();
-        pleaseWaitDialog->show();
-
+    // if there is a calculation running - do nothing
+    if(calculating){
+        return;
     }
+
+    // deletes all existing calculating objects and threads
+    hashCalculatorVector.clear();
+    threadVector.clear();
+
+    // gets the name of the file, which checksum/s the user want
+    fileName = ui->lineEdit->text();
+
+    // if the file name is empty string, display a warning box and return
+    if(fileName.size() == 0){
+        QMessageBox::warning(this, "File Not Selected", "There is no file selected");
+        return;
+    }
+
+    // if there is no file with this name, display a warning box and return
+    QFileInfo check(fileName);
+    if(!check.exists() || !check.isFile()){
+        QMessageBox::warning(this, "File Does Not Exist!", fileName + "\n does not exist");
+        return;
+    }
+
+    // starts calculating if a single hash algorithm is selected
+    for(int i = 0; i < SHA1_AND_MD5; ++i){
+        if( radioButtons[i]->isChecked() ){
+            calculateSingleChecksum( static_cast<QCryptographicHash::Algorithm>(i) );
+        }
+    }
+
+    // starts calculating if the "MD5 and SHA1" option is selected
+    if(radioButtons[SHA1_AND_MD5]->isChecked()){
+        calculate_MD5_and_SHA1();
+    }
+
+    // resets the "Please Wait" dialog to initial state
+    pleaseWaitDialog->reinit();
+
+    // displays the "Please Wait" dialog
+    pleaseWaitDialog->show();
 }
 
 void  MainWindow::onTick()
--- mainwindow.h
+++ mainwindow.h
@@ -12,6 +12,7 @@ namespace Ui {
 class MainWindow;
 }
 
+// indexes for the vector, containing the radioButton pointers
 enum radioButtonIndexes{
     MD4,
     MD5,
@@ -28,8 +29,12 @@ enum radioButtonIndexes{
     COUNT
 };
 
+/**
+ *@brief The application main window.
+ */
 class MainWindow : public QMainWindow
 {
+
     Q_OBJECT
 
 public:
@@ -37,32 +42,62 @@ public:
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    QFileDialog *fileDialog;
 
-    QString fileName;
+    Ui::MainWindow*             ui;                     //!< user interface for the main window
+    QFileDialog*                fileDialog;             //!< file dialog, opens when the "Select File..." button is clicked
+    QString                     fileName;               //!< full path to the file, which checksum/s will be calculated
+    PleaseWaitDialog*           pleaseWaitDialog;       //!< dialog, indicating the checksum calculation is running at the moment
+    ResultDialog*               resultDialog;           //!< dialog, displaying the checksum
+    QVector<HashCalculator*>    hashCalculatorVector;   //!< objects for calculating checksums. Each object calculates only one checksum, and runs in different thread
+    QVector<QThread*>           threadVector;           //!< vector of threads, in which HashCalculator objects are calculating
+    QTimer*                     threadTimer;            //!< timer, which starts the check for running threads
+    QVector<class QRadioButton*>radioButtons;           //!< vector of pointers to all radio buttons in the interface ( for easier access )
+    bool calculating;                                   //!< flag, indicating whether a calculation is running. If it's up, "Calculate Hash" button is inactive.
 
-    PleaseWaitDialog *pleaseWaitDialog;
-    ResultDialog    *resultDialog;
-
-    QVector<HashCalculator*> hashCalculatorVector;
-    QVector<QThread*> threadVector;
-
-    QTimer *threadTimer;
-
-    QVector<class QRadioButton*> radioButtons;
-    class QGroupBox* groupBox;
-
-    bool calculating;
-
+    /**
+     * @brief calculate_MD5_and_SHA1 - Calculates both MD5 and SHA1 checksums, and displays them in a single ResultDialog
+     */
     void calculate_MD5_and_SHA1();
+
+    /**
+     * @brief calculateSingleChecksum - Calculates a single checksum, and displays the result in a ResultDialog
+     *
+     * @param algoritm - The algorithm to be used for the hash calculation
+     */
     void calculateSingleChecksum(QCryptographicHash::Algorithm algoritm);
 
 private slots:
+
+    /**
+     * @brief onFileSelected - Triggered when a file is selected from the file dialog.
+     * Adds the full path to the selected file to the line edit of the Main Window.
+     *
+     * @param path - full path to the selected file,
+     */
     void onFileSelected(const QString & path);
+
+    /**
+     * @brief onPleaseWaitDialogClose - Triggered when the "Cancel" button of the "Please Wait" dialog box is pressed.
+     * Aborts the checksum calculation.
+     */
     void onPleaseWaitDialogClose();
+
+    /**
+     * @brief on_toolButton_clicked - Triggered when the "Select File..." button is clicked.
+     * Opens the file dialog.
+     */
     void on_toolButton_clicked();
+
+    /**
+     * @brief on_pushButton_clicked - Triggered when the "Calculate Hash" button is clicked;
+     * Starts calculating the checksum/s for the corresponding radio button
+     */
     void on_pushButton_clicked();
+
+    /**
+     * @brief onTick - Triggered when the threadTimer emits timeout()
+     * Checks for running threads, and if there are none, displays the result of the calculation.
+     */
     void onTick();
 };
 
